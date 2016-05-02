b198a8e1c9d299ab9bf38902236105439ace0e94
pixelhijack <pothattila@gmail.com>
2016-05-02 06:36:19 +0000

    LEVELS: into the woods platforms

--- pre2.js
+++ pre2.js
@@ -3547,13 +3547,122 @@
 	    }
 	  ],
 	  platforms: [
-	    
+	    {
+	      img: atlas.PLATFORM_WOOD,
+	      behaviour: 'shuttle', // null, fall, moveTo, shuttle
+	      x: 706,
+	      y: 214,
+	      fallTimeout: 1000,
+	      restoreTimeout: 2000,
+	      moveTo: {
+	        x: 177,
+	        y: 214,
+	        timeout: 10000,
+	        easing: 'Cubic'
+	      }
+	    },
+	    {
+	      img: atlas.PLATFORM_WOOD,
+	      behaviour: 'fall', // null, fall, moveTo, shuttle
+	      x: 626,
+	      y: 386,
+	      fallTimeout: 1000,
+	      restoreTimeout: 2000,
+	      moveTo: {
+	        x: 177,
+	        y: 214,
+	        timeout: 10000,
+	        easing: 'Cubic'
+	      }
+	    },
+	    {
+	      img: atlas.PLATFORM_WOOD,
+	      behaviour: 'fall', // null, fall, moveTo, shuttle
+	      x: 232,
+	      y: 273,
+	      fallTimeout: 1000,
+	      restoreTimeout: 2000,
+	      moveTo: {
+	        x: 177,
+	        y: 214,
+	        timeout: 10000,
+	        easing: 'Cubic'
+	      }
+	    },
+	    {
+	      img: atlas.PLATFORM_WOOD,
+	      behaviour: 'fall', // null, fall, moveTo, shuttle
+	      x: 684,
+	      y: 298,
+	      fallTimeout: 1000,
+	      restoreTimeout: 2000,
+	      moveTo: {
+	        x: 177,
+	        y: 214,
+	        timeout: 10000,
+	        easing: 'Cubic'
+	      }
+	    }
 	  ],
 	  bonus: [
 	    
 	  ],
 	  enemies: [
-	    
+	    {
+	      type: 'native',
+	      number: 1,
+	      lifespan: Infinity,
+	      revive: false,
+	      origin: {
+	        x: 321,
+	        y: 248
+	      },
+	      boundTo: {
+	        x1: 321,
+	        x2: 518
+	      }
+	    },
+	    {
+	      type: 'native',
+	      number: 1,
+	      lifespan: Infinity,
+	      revive: false,
+	      origin: {
+	        x: 400,
+	        y: 113
+	      },
+	      boundTo: {
+	        x1: 400,
+	        x2: 523
+	      }
+	    },
+	    {
+	      type: 'native',
+	      number: 1,
+	      lifespan: Infinity,
+	      revive: false,
+	      origin: {
+	        x: 736,
+	        y: 112
+	      },
+	      boundTo: {
+	        x1: 736,
+	        x2: 862
+	      }
+	    },
+	    {
+	      type: 'ptero',
+	      number: 1,
+	      lifespan: Infinity,
+	      revive: 1000,
+	      origin: {
+	        x: 864,
+	        y: 4
+	      },
+	      boundTo: {
+	
+	      }
+	    }
 	  ]
 	};
 	
