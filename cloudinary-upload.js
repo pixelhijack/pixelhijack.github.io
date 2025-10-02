02a9ae79f71e61d6ed8d1813d5a78b6552a78839
pothattila <pothattila@gmail.com>
2025-10-02 21:05:39 +0200

    more subroutes

--- cloudinary-upload.js
+++ cloudinary-upload.js
@@ -18,7 +18,9 @@ if (!process.env.CLOUDINARY_API_KEY) {
 // Define the base folder for local images (must be inside public)
 const baseFolderPath = './public/img';
 // Choose which subfolder to process
-const photoFolderPath = path.join(baseFolderPath, 'analog'); 
+// subfolder: 'analog' or '' for all images in img
+//const photoFolderPath = path.join(baseFolderPath, 'analog'); 
+const photoFolderPath = path.join(baseFolderPath, ''); 
 
 // The output JSON file for uploaded records
 const uploadedFilePath = './uploaded.json';
