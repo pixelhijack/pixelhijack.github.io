b0c8b7d708b0b6bb32adb2dff79135b353ce5d29
pothattila <pothattila@gmail.com>
2025-10-30 11:56:55 +0100

    neurodiv: checkboxes interest form

--- server.js
+++ server.js
@@ -59,6 +59,10 @@ app.post('/form', async (req, res) => {
       sheetName = 'pothattila.com: workshop';
       // Order must match the sheet's column order
       values = [timestamp, formData.name, formData.email];
+  } else if (intent === 'neurodiv: segment') {
+      sheetName = 'neurodiv: segment';
+      // Order must match the sheet's column order
+      values = [timestamp, formData.name, formData.email, formData.role, formData.message, JSON.stringify(formData.checkboxes)];
   } else {
       return res.status(400).send('Invalid form type provided.');
   }
